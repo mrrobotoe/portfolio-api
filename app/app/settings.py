bd9dd826c361e0ae6b6d053cc0e70f03b1faedd4
mrrobotoe <aldo.martinez@my.utsa.edu>
2024-07-06 21:49:35 -0500

    fixed conf

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get("DEBUG", 0)))
+DEBUG = bool(int(os.environ.get("DJANGO_DEBUG", 0)))
 
 # ALLOWED_HOSTS = ["192.168.0.51", "192.168.0.181", "127.0.0.1"]
 ALLOWED_HOSTS = []
--- docker/proxy/nginx/default.conf.tpl
+++ docker/proxy/nginx/default.conf.tpl
@@ -6,14 +6,7 @@ server {
     root /vol/www/;
   }
 
-  location /static {
-    alias /vol/static;
-  }
-
   location / {
-    uwsgi_pass            ${APP_HOST}:${APP_PORT};
-    include               /etc/nginx/uwsgi_params;
-    client_max_body_size  10M;
     return 301 https://$host$request_uri;
   }
 }
