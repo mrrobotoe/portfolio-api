78ab400b455cef7f70056c28140afa6b7ee1cd0a
mrrobotoe <aldo.martinez@my.utsa.edu>
2024-07-12 10:17:57 -0500

    updated allowed_hosts for ecs

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from socket import gethostname, gethostbyname
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,23 +21,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "changeme")
+SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get("DJANGO_DEBUG", 0)))
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = (
-#     [] if DEBUG else os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
-# )
-
 ALLOWED_HOSTS.extend(
     filter(
         None,
         os.environ.get("ALLOWED_HOSTS", "").split(","),
     )
 )
-
+if os.environ.get("AWS_EXECUTION_ENV"):
+    ALLOWED_HOSTS.append(gethostbyname(gethostname()))
 
 # Application definition
 
